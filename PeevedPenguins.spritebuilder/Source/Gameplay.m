6c86243918d34c74c96fc069605d59c35ef315fc
Vincent Pace <vincentpace@Vincents-MacBook-Pro.local>
2014-07-13 20:26:37 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -45,9 +45,14 @@
     [penguin.physicsBody applyForce:force];
     
     // ensure followed object is in visible are when starting
+    /* self.position = ccp(0, 0);
+    CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
+    [self runAction:follow]; */
+    
+    // same as above but applying the scroll to the content node instead of the whole thing
     self.position = ccp(0, 0);
     CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
-    [self runAction:follow];
+    [_contentNode runAction:follow];
 }
 
 - (void)retry {
