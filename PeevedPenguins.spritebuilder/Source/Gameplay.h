14eefa5af3218a7e493fa0c2f808c267c80a8daf
Vincent Pace <vincentpace@Vincents-MacBook-Pro.local>
2014-07-13 19:36:13 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Vincent Pace on 14.07.13.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
